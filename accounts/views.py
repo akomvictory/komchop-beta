527968f8510aeed24efe1bc8b7cbef73c30342a3
akom <akom.victory@gmail.com>
2022-08-26 19:19:19 -0700

    step 31 lesson 6  Hash The Password From Form

--- accounts/views.py
+++ accounts/views.py
@@ -7,7 +7,9 @@ def registerUser(request):
     if request.method == 'POST': # we check the request type from user
         form = UserForm(request.POST) #we use the the request body as our form data
         if form.is_valid():
+            password = form.cleaned_data['password'] #here we clean password value from user
             user = form.save(commit=False) # here the form is ready to be save but not yet and we can assign values to the user object created
+            user.set_password(password) # here we hash the user provided password
             user.role = User.CUSTOMER # here we use the user object we created to assigned newly registered user as customer defaultly
             user.save() # here we save the user registerUser
             return redirect('registerUser')       # this will execute if request type from user is POST
