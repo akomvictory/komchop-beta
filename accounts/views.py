e5779e001902864ca3de4e92a5cd4014e6aa88c3
akom <akom.victory@gmail.com>
2022-08-27 13:22:52 -0700

    step 38 section 7 Vendor Registration Template

--- accounts/views.py
+++ accounts/views.py
@@ -26,3 +26,6 @@ def registerUser(request):
     }
 
     return render(request, 'accounts/registerUser.html', context)
+    
+def registerVendor(request):
+    return render(request, 'accounts/registerVendor.html')    
